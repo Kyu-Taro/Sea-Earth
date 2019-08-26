8b5e7e7d1f62f539a472095e8faa7f32d5244415
Kyu Taro <kyutaro@Kyu.local>
2019-08-26 09:53:37 +0900

    commit

---
+++ database/migrations/2019_08_25_084310_create_texts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTextsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('shop_id')->notnull();
+            $table->string('text')->notnull();
+            $table->string('img')->notnull();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('texts');
+    }
+}
