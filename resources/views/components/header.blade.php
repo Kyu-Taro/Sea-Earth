79d02a2313918ee4328944e09b6efa7d353380eb
Kyu Taro <kyutaro@Kyu.local>
2019-09-02 10:02:46 +0900

    commit

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,11 @@
 <header>
-    <h1 class="top-logo"><a href="/sea-earth_laravel/public/">Sea&Earth</a></h1>
+    <h1 class="top-logo"><a href="/">Sea&Earth</a></h1>
     <nav class="header-nav">
         <ul class="header-ul">
             <li><a href="{{ $link1 }}">{{ $nav1 }}</a></li>
             <li><a href="{{ $link2 }}">{{ $nav2 }}</a></li>
             <li><a href="{{ $link3 }}">{{ $nav3 }}</a></li>
             <li><a href="{{ $link4 }}">{{ $nav4 }}</a></li>
-            {{-- <li><a href="{{ $link5 }}">{{ $nav5 }}</a></li> --}}
         </ul>
     </nav>
 </header>
