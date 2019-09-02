c08ee39bc514569e29dbc8c8350bbf5505985bdc
Kyu Taro <kyutaro@Kyu.local>
2019-09-02 16:20:40 +0900

    commitQ

--- resources/views/template/textDetailParent.blade.php
+++ resources/views/template/textDetailParent.blade.php
@@ -8,6 +8,13 @@
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/cdebaefb03.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-9538746713975091",
+            enable_page_level_ads: true
+        });
+    </script>
     <title>@yield('title')</title>
 </head>
 <body>
