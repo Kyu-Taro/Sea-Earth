22df2b0484137d3f6e7d63a04304b31fa88d6aca
Kyu Taro <kyutaro@Kyu.local>
2019-09-02 10:27:55 +0900

    ログインレスポンシブ

--- resources/views/template/form.blade.php
+++ resources/views/template/form.blade.php
@@ -7,9 +7,22 @@
     <link rel="stylesheet" href="@yield('href')" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/cdebaefb03.js"></script>
     <title>@yield('title')</title>
 </head>
 <body>
+    <i class="fas fa-bars"></i>
+    <i class="fas fa-undo"></i>
+    <div class="menu">
+        <span class="comment">Menu</span>
+        <ul class="menu-ul">
+            <li><a href="/">Top</a></li>
+            <li><a href="register">Register</a></li>
+            <li><a href="text">Photos</a></li>
+            <li><a href="mypage">Mypage</a></li>
+        </ul>
+    </div>
+
     @yield('header')
 
     <section class="form-contents">
