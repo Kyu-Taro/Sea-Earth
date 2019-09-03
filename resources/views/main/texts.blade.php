073b2c039397879e185ea4ef0fa636d55b7c0410
Kyu Taro <kyutaro@Kyu.local>
2019-09-03 10:22:34 +0900

    commit

--- resources/views/main/texts.blade.php
+++ resources/views/main/texts.blade.php
@@ -45,11 +45,12 @@
                 {{ $text->text }}
             </div>
         </div>
-        <a href="https://px.a8.net/svt/ejp?a8mat=35NQEK+98TBXU+AD2+2HCQGX" rel="nofollow">
-            <img border="0" width="120" height="60" alt="" src="https://www22.a8.net/svt/bgt?aid=190903916559&wid=001&eno=01&mid=s00000001343015008000&mc=1"></a>
-            <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=35NQEK+98TBXU+AD2+2HCQGX" alt="">
     @endforeach
 @endsection
+
+<a href="https://px.a8.net/svt/ejp?a8mat=35NQEK+98TBXU+AD2+2HCY6P" rel="nofollow">
+    <img border="0" width="234" height="60" alt="" src="https://www22.a8.net/svt/bgt?aid=190903916559&wid=001&eno=01&mid=s00000001343015009000&mc=1"></a>
+    <img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=35NQEK+98TBXU+AD2+2HCY6P" alt="">
 
 @section('footer')
     @component('components.footer')
